920cd69ac91d5333d283408fdb313d67430dca7b
Your Name <you@example.com>
2018-01-23 00:01:22 +0300

    create folder

---
+++ main.js
@@ -0,0 +1,35 @@
+(function () {
+    "use strict";
+
+    // Diamonds	Бубны
+    // Hearts	Червы/черви
+    // Spades	Пики
+    // Clubs	Трефы
+    // Ace	Туз = 14
+    // King	Король = 13
+    // Queen	Дама = 12
+    // Jack	Валеt = 11
+
+    let vasiaHand = [];
+    let petiaHand = [];
+
+    function Card(name, suit, value, isTrump){
+        this.name = name;
+        this.suit = suit;
+        this.value = value;
+        this.tramp = isTrump;
+    }
+// var card = new Card("Jack", "Hearts", 11, false);
+
+
+
+
+
+
+
+
+
+
+
+
+}());
