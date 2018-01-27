f51ddf45c89e3f4d138213c8708ca3c375215167
Your Name <you@example.com>
2018-01-27 20:55:15 +0300

    done

--- main.js
+++ main.js
@@ -1,40 +1,22 @@
 // (function () {
 "use strict";
-//
-// let myPromise = new Promise(function(resolve, reject){
-//
-//     return setTimeout(function(){
-//
-//         resolve("tratata");
-//         let a = 2;
-//         return a;
-//     }, 3000);
-//
-// });
-//
-// myPromise.then(
-//     function(result){
-//         console.log(result);
-//     },
-//     function(error){
-//         console.log("error: " + error);
-//     }
-// );
+
 
 const COUNT_SUIT = 4;
 const SUITS = ["hearts", "diamonds", "clubs", "spades"];
-const VALUE_DECK = 36;
 const MAX_VALUE_CARDS = 14;
 const MIN_VALUE_CARDS = 6;
+const VALUE_DECK = 36;
 const VALUE_PLAYER = 2;
 
 
 let game;
-
+let playerDeckVal = VALUE_DECK / VALUE_PLAYER;
 
 function getRandomVal(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
+
 function setName(obj) {
     let objName;
     if (obj.value < 11 && obj.value > 5) {
@@ -59,17 +41,21 @@ function setName(obj) {
 
 
 }
+
 function setIsTrump(obj) {
     return obj.suit.toLowerCase() === game.trump.toLowerCase();
 }
 
+function setClassList(obj) {
+    return obj.suit + " " + "value-" + obj.value;
+}
 
 function Game(playerName1, playerName2) {
     this.player1 = {
         name: playerName1,
-        score: new Array(VALUE_DECK / VALUE_PLAYER).fill(0),
-        allScore: function(){
-            return this.score.reduce(function(sum, current) {
+        score: new Array(playerDeckVal).fill(0),
+        allScore: function () {
+            return this.score.reduce(function (sum, current) {
                 return sum + current;
             });
         },
@@ -77,9 +63,9 @@ function Game(playerName1, playerName2) {
     };
     this.player2 = {
         name: playerName2,
-        score: new Array(VALUE_DECK / VALUE_PLAYER).fill(0),
-        allScore: function(){
-            return this.score.reduce(function(sum, current) {
+        score: new Array(playerDeckVal).fill(0),
+        allScore: function () {
+            return this.score.reduce(function (sum, current) {
                 return sum + current;
             });
         },
@@ -89,11 +75,13 @@ function Game(playerName1, playerName2) {
     this.deckCards = [];
     this.shuffleDeckCards = [];
 }
+
 function Card(suit, value) {
     this.suit = suit;
     this.value = value;
     this.isTramp = setIsTrump(this);
     this.name = setName(this);
+    this.classList = setClassList(this);
 }
 
 function createDeck() {
@@ -106,50 +94,55 @@ function createDeck() {
         }
     }
 }
+
 function shuffleCards() {
     while (game.deckCards.length) {
         game.shuffleDeckCards = game.shuffleDeckCards.concat(game.deckCards.splice(getRandomVal(0, game.deckCards.length), 1));
     }
 }
+
 function splitShuffleDeckCards() {
-    game.player1.deck = game.player1.deck.concat(game.shuffleDeckCards.splice(0, VALUE_DECK / VALUE_PLAYER));
-    game.player2.deck = game.player2.deck.concat(game.shuffleDeckCards.splice(0));
+    game.player1.deck = game.player1.deck.concat(game.shuffleDeckCards.splice(0, playerDeckVal));
+    game.player2.deck = game.player2.deck.concat(game.shuffleDeckCards.splice(0, playerDeckVal));
 }
-function competeRaund() {
-    for (let i = 0; i < VALUE_DECK / VALUE_PLAYER; i++) {
-    if (game.player1.deck[i].isTramp > game.player2.deck[i].isTramp) {
-        game.player1.score[i] = 1;
-    } else if (game.player1.deck[i].isTramp < game.player2.deck[i].isTramp) {
-        game.player2.score[i] = 1;
-    } else if (game.player1.deck[i].isTramp && game.player2.deck[i].isTramp) {
-        if (game.player1.deck[i].value > game.player2.deck[i].value) {
-            game.player1.score[i] = 1;
+
+function competeRaunds() {
+    for (let i = playerDeckVal - 1; i >= 0; i--) {
+        let n = playerDeckVal - 1 - i;
+        if (game.player1.deck[i].isTramp > game.player2.deck[i].isTramp) {
+            game.player1.score[n] = 1;
+        } else if (game.player1.deck[i].isTramp < game.player2.deck[i].isTramp) {
+            game.player2.score[n] = 1;
+        } else if (game.player1.deck[i].isTramp && game.player2.deck[i].isTramp) {
+            if (game.player1.deck[i].value > game.player2.deck[i].value) {
+                game.player1.score[n] = 1;
+            } else {
+                game.player2.score[n] = 1;
+            }
+
         } else {
-            game.player2.score[i] = 1;
+            if (game.player1.deck[i].value > game.player2.deck[i].value) {
+                game.player1.score[n] = 1;
+            } else if (game.player1.deck[i].value < game.player2.deck[i].value) {
+                game.player2.score[n] = 1;
+            }
         }
+    }
+}
 
+function printWinner() {
+    let winnerName;
+    if (game.player1.allScore() > game.player2.allScore()) {
+        winnerName = game.player1.name;
+    } else if (game.player1.allScore() < game.player2.allScore()) {
+        winnerName = game.player2.name;
     } else {
-        if (game.player1.deck[i].value > game.player2.deck[i].value) {
-            game.player1.score[i] = 1;
-        } else if (game.player1.deck[i].value < game.player2.deck[i].value) {
-            game.player2.score[i] = 1;
-        }
+        winnerName = "Ничья!";
     }
-    }
-}
-function printWinner(){
-let winnerName;
-if (game.player1.allScore() > game.player2.allScore()){
-    winnerName = game.player1.name;
-} else if(game.player1.allScore() < game.player2.allScore()){
-    winnerName = game.player2.name;
-} else {
-    winnerName = "Ничья!";
-}
 
-document.querySelector(".winner-name").textContent = winnerName;
-document.querySelector(".score").textContent = game.player1.allScore() + " : " + game.player2.allScore();
-document.querySelector(".suit").textContent = game.trump;
+    document.querySelector(".winner-name").textContent = winnerName;
+    document.querySelector(".score").textContent = game.player1.allScore() + " : " + game.player2.allScore();
+    document.querySelector(".suit").textContent = game.trump;
 }
 
 
@@ -158,35 +151,47 @@ document.querySelector(".suit").textContent = game.trump;
 
 
 
-let myPromise = new Promise(function(resolve, reject){
-     document.querySelector(".btn-start").addEventListener("click", function() {
-         game = new Game("Petia", "Vasia");
-         createDeck();
-         shuffleCards();
-         splitShuffleDeckCards();
-         competeRaund();
-         printWinner();
-         resolve(true);
-     });
+let myPromise = new Promise(function (resolve, reject) {
+    document.querySelector(".btn-start").addEventListener("click", function () {
+        game = new Game("Petia", "Vasia");
+        createDeck();
+        shuffleCards();
+        splitShuffleDeckCards();
+        competeRaunds();
+        printWinner();
+        resolve(true);
+    });
 }).then(
-    function(result){
-        let i = 0;
-        function showRow(){
-            if (i > VALUE_DECK/VALUE_PLAYER - 1) {return;}
+    function (result) {
+        let i = playerDeckVal - 1;
+
+        function showRow() {
+            if (!i) {
+                return;
+            }
+
             let row = document.createElement("tr");
+
             let td1 = document.createElement("td");
-            console.log(i);
-            td1.classList.add(game.player1.deck[i].name);
+            let cardPlayer1 = document.createElement("div");
+            cardPlayer1.classList.add(game.player1.deck[i].suit);
+            cardPlayer1.classList.add("value-" + game.player1.deck[i].value);
+
             let td2 = document.createElement("td");
-            td2.classList.add(game.player2.deck[i].name);
+            let cardPlayer2 = document.createElement("div");
+            cardPlayer2.classList.add(game.player2.deck[i].suit);
+            cardPlayer2.classList.add("value-" + game.player2.deck[i].value);
+
+            td1.appendChild(cardPlayer1);
             row.appendChild(td1);
+            td2.appendChild(cardPlayer2);
             row.appendChild(td2);
             document.querySelector(".game-results").appendChild(row);
-            i++;
+            i--;
         }
         document.querySelector(".show-row").addEventListener("click", showRow);
     },
-    function(error){
+    function (error) {
         console.log("error: " + error);
     }
 );
